82ccead9e4d1a58a1891522f3a19cf45719bf557
jllopezpino <jllopezpino@gmail.com>
2014-05-07 10:10:47 +0200

    Bugs fixed. First working version of the code ready

--- pmml/PMMLEvaluator.java
+++ pmml/PMMLEvaluator.java
@@ -41,6 +41,7 @@ import eu.stratosphere.api.java.DataSet;
 import eu.stratosphere.api.java.ExecutionEnvironment;
 import eu.stratosphere.api.java.functions.MapFunction;
 import eu.stratosphere.api.java.tuple.Tuple2;
+import eu.stratosphere.api.java.tuple.Tuple3;
 import eu.stratosphere.configuration.Configuration;
 
 @SuppressWarnings("serial")
@@ -51,18 +52,26 @@ public class PMMLEvaluator {
 	 */
 	public static class Instance implements Serializable {
 
+		private static String INSTANCE_SEPARATOR = ",";
+		
 		private double[] features;
+		private int id;
 		
 		public Instance() {}
+		
+		public Instance(String line){
+			
+			String[] array = line.split(INSTANCE_SEPARATOR, 2);
+			
+			id = new Integer(array[0]).intValue();
+			setFeatures(array[1]);
+			
+		}
 
-		public Instance(String line) {
-			String[] inputFeatures = line.split(",");
+		public Instance(int id, String featuresLine) {
+			this.id = id;
 
-			features = new double[inputFeatures.length];
-
-			for (int i = 0; i < inputFeatures.length; i++) {
-				features[i] = Double.parseDouble(inputFeatures[i]);
-			}
+			setFeatures(featuresLine);
 		}
 
 		public Instance(double[] features) {
@@ -71,8 +80,13 @@ public class PMMLEvaluator {
 
 		@Override
 		public String toString() {
+			return id + "," + this.getStringFeatures();
+		}
+		
+		public String getStringFeatures(){
 			int i;
 			StringBuffer resultingString = new StringBuffer();
+			
 			for (i = 0; i < features.length - 1; i++) {
 				resultingString.append(features[i] + ",");
 			}
@@ -83,6 +97,21 @@ public class PMMLEvaluator {
 
 		public double[] getFeatures() {
 			return features;
+		}
+		
+		public void setFeatures(String featuresLine){
+			
+			String[] inputFeatures = featuresLine.split(",");
+			
+			features = new double[inputFeatures.length];
+
+			for (int i = 0; i < inputFeatures.length; i++) {
+				features[i] = Double.parseDouble(inputFeatures[i]);
+			}
+		}
+		
+		public int getId(){
+			return id;
 		}
 
 		// we assume that the features are in the same order as the fields
@@ -110,7 +139,7 @@ public class PMMLEvaluator {
 	 *
 	 */
 	public static final class Classify extends
-			MapFunction<Instance, Tuple2<String, String>> {
+			MapFunction<String, Tuple3<Integer, String, String>> {
 		private static final long serialVersionUID = 1L;
 
 		private Evaluator evaluator = null;
@@ -175,18 +204,48 @@ public class PMMLEvaluator {
 			setPredictedField();
 
 		}
+		
+		private Map<FieldName, FieldValue> tuple2Map(Tuple2<Integer, String> tuple, Evaluator evaluator) {
+
+			List<FieldName> names = evaluator.getActiveFields();
+
+			Map<FieldName, FieldValue> result = new LinkedHashMap<FieldName, FieldValue>();
+			
+			String line = tuple.getField(1);
+			String[] inputFeatures = line.split(",");
+
+			for (int i = 0; i < names.size() - 1; i++) {
+				FieldName name = names.get(i);
+				Double feature = Double.parseDouble(inputFeatures[i]);
+				
+				System.out.println("Feature read: " + feature);
+
+				FieldValue value = EvaluatorUtil.prepare(evaluator, name, feature);
+				result.put(name, value);
+			}
+			
+			
+
+			return result;
+		
+		}
 
 		@Override
-		public Tuple2<String, String> map(Instance p) throws Exception {
+		public Tuple3<Integer, String, String> map(String observation) throws Exception {
 
+			Instance p = new Instance(observation);
+			
 			Map<FieldName, FieldValue> arguments = p.toMap(evaluator);
 
 			Map<FieldName, ?> classified = evaluator.evaluate(arguments);
 
 			String label = String.valueOf(classified.get(predictedField));
+			
+			Integer returnId = p.getId();
+			String returnFeatures = p.getStringFeatures();
 
 			// emit a new tuple with the label
-			return (new Tuple2<String, String>(label, p.toString()));
+			return (new Tuple3<Integer, String, String>(returnId, label, returnFeatures));
 
 		}
 	}
@@ -204,21 +263,37 @@ public class PMMLEvaluator {
 
 		ExecutionEnvironment env = ExecutionEnvironment
 				.getExecutionEnvironment();
+		
+
+		Classify classifyOperator = new Classify(model);
+		
 
 		// I think I should use this
 		// env.readCsvFile(filePath)
 
-		DataSet<Instance> instancesToClassify = env.fromElements(new Instance(
-				"5.1, 3.5, 1.4, 0.2"), new Instance("4.9, 3.0, 1.4, 0.2"),
-				new Instance("7.0, 3.2, 4.7, 1.4"), new Instance(
-						"6.4, 3.2, 4.5, 1.5"), new Instance("8.0, 3.3, 3, 2"));
-
-		Classify classifyOperator = new Classify(model);
 		
-		DataSet<Tuple2<String, String>> resultData = instancesToClassify
-				.map(classifyOperator);
 
-		resultData.writeAsText(output);
+		
+		DataSet<String> inputData = env.readTextFile(input);
+		
+		DataSet<Tuple3<Integer, String, String>> resultData = inputData.map(classifyOperator);
+		
+		
+		
+		
+		
+//		DataSet<Instance> instancesToClassify = env.fromElements(
+//				new Instance("1, 5.1, 3.5, 1.4, 0.2"), 
+//				new Instance("2, 4.9, 3.0, 1.4, 0.2"),
+//				new Instance("3, 7.0, 3.2, 4.7, 1.4"), 
+//				new Instance("4, 6.4, 3.2, 4.5, 1.5"), 
+//				new Instance("5, 8.0, 3.3, 3, 2"));
+//		
+//		
+//		DataSet<Tuple3<Integer, String, String>> resultData = instancesToClassify
+//				.map(classifyOperator);
+
+		resultData.writeAsCsv(output);
 		
 		
 		
